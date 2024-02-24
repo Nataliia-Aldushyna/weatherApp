470f7a05872100597d48d28078b1e17951906d6d
Nataliia-Aldushyna <121820938+Nataliia-Aldushyna@users.noreply.github.com>
2024-02-24 11:43:54 +0100

    Update cityForecast.js

--- js/cityForecast.js
+++ js/cityForecast.js
@@ -25,7 +25,7 @@ class cityForecast {
         let finalList = forecastList.filter(forecast => {
             let date = new Date(forecast.dt * 1000);
             console.log(date)
-            return date.getHours() == 15;
+            return date.getHours() == 16;
         })
 
         console.log(finalList)
